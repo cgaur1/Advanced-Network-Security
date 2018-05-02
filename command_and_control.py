7cb6b2b600ef8f739717c599009138b7b7069e08
Chanakya Gaur <31703683+cgaur1@users.noreply.github.com>
2018-05-02 22:56:22 +0000

    Add files via upload

--- command_and_control.py
+++ command_and_control.py
@@ -1,5 +1,5 @@
 import playground
-import asyncio, os
+import time, asyncio, os
 import re
 
 from playground.common.io.ui.CLIShell import CLIShell, AdvancedStdio
@@ -12,6 +12,7 @@ matrix = []
 initialmap = 0
 m_x = m_y = 0
 substring = "Could not"
+firstmap = 0
 
 if not initialmap:
     for i in range(100):
@@ -96,14 +97,17 @@ class RemoteConsole(CLIShell):
         reprogramHandler    = Command("reprogram",
                                       "reprogram bot. Careful. Don't BRICK it.",
                                       self._reprogramCommand)
+        downloadBrainHandler= Command("download_brain",
+                                      "download the bot's current brain as a tar ball.",
+                                      self._downloadBrainCommand)
         self.registerCommand(switchobjectHandler)
         self.registerCommand(sendcommandHandler)
         self.registerCommand(listobjectsHandler)
         self.registerCommand(reprogramHandler)
+        self.registerCommand(downloadBrainHandler)
 
         coro = playground.create_server(lambda: RemoteControlProtocol(self), port=port, family=serverFamily)
         asyncio.ensure_future(coro)
-
 
     def addConnection(self, protocol):
         self._protocolId += 1
@@ -120,11 +124,10 @@ class RemoteConsole(CLIShell):
         self.transport.write("Network Failure: {}\n\n".format(e))
 
     def createObjectDisplay(self, objectData, indent=""):
-       s = ""
-       for key, value in objectData:
-           s += "{}{}: {}\n".format(indent, key, value)
-       return s 
-    
+        s = ""
+        for key, value in objectData:
+            s += "{}{}: {}\n".format(indent, key, value)
+        return s
 
     def createScanResultsDisplay(self, scanResults):
         mapPart = ""
@@ -156,20 +159,29 @@ class RemoteConsole(CLIShell):
             elif terrain == "water":
                 mapLine += "="
                 matrix[y][x] = "="
+
         mapPart = mapLine + "\n" + mapPart + "\n"
         self.createMapResultDisplay(matrix[::-1])
         return mapPart + textPart + "\n"
-    
+
     def createMapResultDisplay(self,print_map):
         global map
         global m_x
         global m_y
+        global firstmap
         map = ""
-        for i in range(100):
-            for j in range(100):
-                map += str(print_map[i][j])
-            map += "\n"
-        print(map)
+        if not firstmap:
+            for i in range(100):
+                for j in range(100):
+                    map += str(print_map[i][j])
+                map += "\n"
+            print(map)
+        else:
+            for i in range(m_x):
+                for j in range(m_y):
+                    map += str(print_map[i][j])
+                map += "\n"
+            print(map)
         
 
     def handleNetworkData(self, protocol, data):
@@ -190,16 +202,16 @@ class RemoteConsole(CLIShell):
         elif isinstance(data, translations.ScanResponse):
             self.transport.write(self.createScanResultsDisplay(data.scanResults))
             self.transport.write("\n")
-            global m_x
-            global m_y
         elif isinstance(data, translations.MoveCompleteEvent):
             self.transport.write("Move result: {}\n\n".format(data.message))
+            global firstmap
             substring = "Could not"
             coordinates = []
             if substring in data.message:
                 coordinates = re.findall(r'\d+', data.message)
                 m_x = int(coordinates[-2])
                 m_y = int(coordinates[-1])
+                firstmap = 1
                 #self.createemptymatrix(m_x,m_y)
         elif isinstance(data, translations.ObjectMoveEvent):
             if data.status == "insert":
@@ -213,15 +225,38 @@ class RemoteConsole(CLIShell):
             self.transport.write("{} hit {} for {} points of damage (took {} points of damage). {}".format(protocol.identifier, data.targetObjectIdentifier, data.targetDamage, data.damage, data.message))
         elif isinstance(data, translations.ReprogramResponse):
             self.transport.write("Reprogram of {} {}. {}\n\n".format(data.path, (data.success and "successful" or "unsuccessful"), data.message))
+        elif isinstance(data, translations.DownloadBrainResponse):
+            tarData = data.data
+            tarName = "brain.{}.tar.gz".format(time.time())
+            with open(tarName, "wb+") as f:
+                f.write(tarData)
+            self.transport.write("Downloaded brain as {}.\n\n".format(tarName))
+        elif isinstance(data, translations.DownloadTargetEvent):
+            tarData = data.brainZip
+            tarName = "object_{}_brain.{}.tar.gz".format(data.targetIdentifier, time.time())
+            with open(tarName, "wb+") as f:
+                f.write(tarData)
+            self.transport.write("Downloaded brain for object {} as {}.\n\n".format(data.targetIdentifier, tarName))
+        elif isinstance(data, translations.ReprogramTargetEvent):
+            self.transport.write("Reprogram of {} {}. {}\n\n".format(data.targetIdentifier, (data.success and "successful" or "unsuccessful"), data.message))
+        elif isinstance(data, translations.RepairCompleteEvent):
+            self.transport.write("Repair complete for object {}. Amount repaired is {} ({}).\n\n".format(data.targetIdentifier,
+                                                                                                     data.amountRepaired,
+                                                                                                     data.message))
+        elif isinstance(data, translations.BuildBotResponse):
+            self.transport.write("Bot built. New ID is {}\n\n".format(data.builtBotIdentifier))
         else:
             self.transport.write("Got {}\n\n".format(data))
         self.transport.refreshDisplay()
-    
+
     def _listCommand(self, writer):
         objKeys = list(self._protocols.keys())
         objKeys.sort()
         for k in objKeys:
-            writer("Object {} at {}\n".format(k, self._protocols[k].transport.get_extra_info("peername")))
+            objectId = self._protocols[k].identifier
+            if objectId == None:
+                objectId = "<Unknown>"
+            writer("{}. Object {} at {}\n".format(k, objectId, self._protocols[k].transport.get_extra_info("peername")))
         writer("\n")
 
     def _switchObjectCommand(self, writer, arg1):
@@ -231,9 +266,25 @@ class RemoteConsole(CLIShell):
             self.prompt = self.STD_PROMPT
         else:
             self._selected = objId
-            writer("Object {} selected\n".format(arg1))
-            self.prompt = "[{}] >> ".format(arg1)
+            writer("Connection {} selected\n".format(arg1))
+            self.prompt = "[connection {}] >> ".format(arg1)
         writer("\n")
+
+    def _downloadBrainCommand(self, writer):
+        protocol = self._protocols.get(self._selected, None)
+        if self._selected is None:
+            writer("No remote object selected.\n\n")
+            return
+        if protocol is None:
+            writer("Selected object no longer available. \n\n")
+            self.prompt = self.STD_PROMPT
+            return
+        cmdObj = translations.DownloadBrainCommand()
+        sendData = protocol.translator.marshallToNetwork(cmdObj)
+        protocol.transport.write(sendData) 
+        writer("Download command sent. \n\n")
+
+
 
     def _reprogramCommand(self, writer, subcmd, *args):
         args = list(args) # convert to list... I want to use .pop
@@ -285,30 +336,114 @@ class RemoteConsole(CLIShell):
             self.prompt = self.STD_PROMPT
             return
 
+        # TODO: have these commands loaded based on type of object
         if cmd == "scan":
-            cmdObj = translations.ScanCommand()
-            sendData = protocol.translator.marshallToNetwork(cmdObj)
-            protocol.transport.write(sendData)
-            writer("Scan Message Sent.\n\n")
+            self._sendCommand_scan(protocol, writer, *args)
         elif cmd == "move":
-            if len(args) != 1:
-                writer("Require a direction argument (N, NE, E, SE, S, SW, W, NW)\n\n")
-                return
-            direction = args[0].lower()
-            if direction in self.DIRECTIONS_SHORT:
-                direction = self.DIRECTIONS_SHORT[direction]
-            if direction not in self.DIRECTIONS_SHORT.values():
-                writer("Unknown direction {}\n\n".format(direction))
-                return
-            cmdObj = translations.MoveCommand(direction)
-            sendData = protocol.translator.marshallToNetwork(cmdObj)
-            protocol.transport.write(sendData)
-            writer("Move Message Sent.\n\n")
+            self._sendCommand_move(protocol, writer, *args)
+        elif cmd == "repair":
+            self._sendCommand_repair(protocol, writer, *args)
         elif cmd == "status":
-            protocol.transport.write(protocol.translator.marshallToNetwork(translations.StatusCommand()))
+            self._sendCommand_status(protocol, writer, *args)
+        elif cmd == "download":
+            self._sendCommand_download(protocol, writer, *args)
+        elif cmd == "reprogram":
+            self._sendCommand_reprogram(protocol, writer, *args)
+        elif cmd == "buildbot":
+            self._sendCommand_buildbot(protocol, writer, *args)
         else:
             writer("Unknown Command {}\n\n".format(cmd))
+    
+    def _sendCommand_buildbot(self, protocol, writer, *args):
+        try:
+            direction = self.DIRECTIONS_SHORT[args[0].lower()]
+            designName, name, address, localBrainPath = args[1:5]
+        except Exception as e:
+            writer("Bad usage. Args must be: direction designName name address brainpath ({})\n\n".format(e))
+            return
+        if not os.path.exists(localBrainPath):
+            writer("No such path {}\n\n".format(localBrainPath))
+            return
+        try:
+            with open(localBrainPath, "rb") as f:
+                cmdObj = translations.BuildBotCommand(direction, designName, name, address, f.read())
+                sendData = protocol.translator.marshallToNetwork(cmdObj)
+                protocol.transport.write(sendData)
+                writer("Build-Bot Message Sent.\n\n")
+        except Exception as e:
+            writer("Could not build bot. {}".format(e))
+            return
+            
+    def _sendCommand_scan(self, protocol, writer, *args):
+        cmdObj = translations.ScanCommand()
+        sendData = protocol.translator.marshallToNetwork(cmdObj)
+        protocol.transport.write(sendData)
+        writer("Scan Message Sent.\n\n")
+        
+    def _sendCommand_move(self, protocol, writer, *args):
+        if len(args) != 1:
+            writer("Require a direction argument (N, NE, E, SE, S, SW, W, NW)\n\n")
+            return
+        direction = args[0].lower()
+        if direction in self.DIRECTIONS_SHORT:
+            direction = self.DIRECTIONS_SHORT[direction]
+        if direction not in self.DIRECTIONS_SHORT.values():
+            writer("Unknown direction {}\n\n".format(direction))
+            return
+        cmdObj = translations.MoveCommand(direction)
+        sendData = protocol.translator.marshallToNetwork(cmdObj)
+        protocol.transport.write(sendData)
+        writer("Move Message Sent.\n\n")
+        
+    def _sendCommand_status(self, protocol, writer, *args):
+        protocol.transport.write(protocol.translator.marshallToNetwork(translations.StatusCommand()))
+        
+    def _sendCommand_download(self, protocol, writer, *args):
+        if len(args) != 1:
+            writer("Requires a target identifier (numeric game object)\n\n")
+            return
+        target = args[0]
+        try:
+            int(target)
+        except:
+            writer("Target identifier must be a numeric identifier\n\n")
+            return
+        sendData = protocol.translator.marshallToNetwork(translations.DownloadTargetCommand(target))
+        protocol.transport.write(sendData)
+        writer("Download target message sent")
+        
+    def _sendCommand_reprogram(self, protocol, writer, *args):
+        try:
+            target, localBrainPath = args
+            int(target)
+        except:
+            writer("reprogram args must be: targetID localBrainPath\n\n")
+            return
+        if not os.path.exists(localBrainPath):
+            writer("No such path {}\n\n".format(localBrainPath))
+            return
+        try:
+            with open(localBrainPath, "rb") as f:
+                sendData = protocol.translator.marshallToNetwork(translations.ReprogramTargetCommand(target, f.read()))
+                protocol.transport.write(sendData)
+                writer("Reprogram target message sent\n\n")
+        except Exception as e:
+            writer("Could not send reprogram command: {}\n\n".format(e))
+            
+    def _sendCommand_repair(self, protocol, writer, *args):
+        try:
+            target,  = args
+            int(target)
+        except:
+            writer("repair args must be: targetID\n\n")
+            return
+        try:
 
+            sendData = protocol.translator.marshallToNetwork(translations.RepairCommand(target))
+            protocol.transport.write(sendData)
+            writer("Repair target message sent\n\n")
+        except Exception as e:
+            writer("Could not send repair command: {}\n\n".format(e))
 
     def stop(self):
         # use list() to make a copy... otherwise closing the protocol
